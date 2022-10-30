8d7ecdce86e2281947c9087ab5029949a8c4963e
barbaraanndrade <babi.anndrade@gmail.com>
2022-10-30 10:27:22 -0300

    exercicios resolvidos

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-02.js
@@ -0,0 +1,23 @@
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof patientAge);
+console.log(typeof isEnrolled);
+
+
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+
+console.log(area);
+
+const perimeter = 5 + 5 + 8 + 8;
+
+console.log(perimeter);
+
